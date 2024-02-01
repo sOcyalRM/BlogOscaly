c5eecec7917eab6484f092e88d131aa78ba3b20e
Oscaly <oscalyromat@icloud.com>
2024-02-01 19:16:05 -0400

    Updates on Media queries. To be continue..

--- core/static/core/js/main.js
+++ core/static/core/js/main.js
@@ -1,25 +1,3 @@
-const images = document.querySelectorAll('.prof_figure');
-console.log(images)
-
-function triggerAnimation(entries){
-    entries.forEach(entry =>{
-        const image = entry.target.querySelector('img');
-
-        image.classList.toggle('unset', entry.isIntersecting);
-    });
-}
-
-const options = {
-    root: null,
-    rootMargin: "-56px",/*Hace mas pequeño el contenedor(el area que determina cuando aparecera y desaparecera la imagen) */
-    threshold: .96
-}
-const observer = new IntersectionObserver(triggerAnimation, options);
-
-images.forEach(image =>{
-    observer.observe(image);
-})
-
 
 //Ejecutando funciones
 document.getElementById("icon-search").addEventListener("click", mostrar_buscador);
